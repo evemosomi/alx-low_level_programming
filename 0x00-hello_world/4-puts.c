269457cb421264b3322c5d1829054ab4a34c5ecf
Everlyne Mosomi <evemosomi@gmail.com>
2022-12-08 18:08:12 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-
 /**
- *
- * main - Entry point
+ * main - code using puts
  *
  * Return: Always 0 (Success)
  */
@@ -10,5 +8,4 @@ int main(void)
 {
 	puts("\"Programming is like building a muiltilingual puzzle");
 	return (0);
-
 }
