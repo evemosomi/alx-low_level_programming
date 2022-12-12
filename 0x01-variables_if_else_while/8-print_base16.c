64a865fb915f431c13f48c8da9647d631534e884
Everlyne Mosomi <evemosomi@gmail.com>
2022-12-12 02:14:36 -0800

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - hexadecimal numbers
+ * Return:0
+ */
+int main(void)
+{
+	int num;
+	char ls;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (ls = 'a'; ls <= 'f'; ls++)
+		putchar(ls);
+
+	putchar('\n');
+	return (0);
+}
+
