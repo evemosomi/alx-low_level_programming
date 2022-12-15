4a03032e2a7dfc09f15e2ad7d526c7911e429d8d
Everlyne Mosomi <evemosomi@gmail.com>
2022-12-15 02:23:47 -0800

    2

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * largest_number -largest of 3 numbers
+ * @a:1st number
+ * @b:2nd number
+ * @c:3rd number
+ * Return:largest number
+ */
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (b > a && b > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+	return (largest);
+}
