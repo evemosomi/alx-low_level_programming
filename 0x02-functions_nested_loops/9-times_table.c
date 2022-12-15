a135ab82a5c86ca78315b4d9a6ec796ea06dc16b
Everlyne Mosomi <evemosomi@gmail.com>
2022-12-14 18:53:31 -0800

    8 andd 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * times_table - prints 9 times
+ */
+void times_table(void)
+{
+	int num, m, prod;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+		for (m = 1; m <= 9; m++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * m;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0);
+		}
+		_putchar('\n');
+	}
