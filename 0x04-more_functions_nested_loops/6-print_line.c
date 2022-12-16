7b19fee33e5e542f9151d1caec4127ce2ee2168c
Everlyne Mosomi <evemosomi@gmail.com>
2022-12-16 13:07:01 -0800

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line -prints line in n times
+ * @n:number of times
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
