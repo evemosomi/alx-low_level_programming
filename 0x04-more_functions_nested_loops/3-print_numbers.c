91e6fe9d3ebb92fb6b2c8bad311368175b1d58c5
Everlyne Mosomi <evemosomi@gmail.com>
2022-12-15 17:45:24 -0800

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - numbers 1-9
+ * Return:void
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c < 10; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
