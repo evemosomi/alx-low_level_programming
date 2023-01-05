6e4f1ca8bf799cb1313beaaada0e11d8ca085c05
Everlyne Mosomi <evemosomi@gmail.com>
2023-01-04 23:12:20 -0800

    3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,12 +6,10 @@
  */
 int factorial(int n)
 {
-	int fact = n;
-
 	if (n < 0)
 		return (-1);
 	else if (n >= 0 && n <= 1)
 		return (1);
 	else
-		return (fact * fact - 1);
+		return (n * factorial(n - 1));
 }
