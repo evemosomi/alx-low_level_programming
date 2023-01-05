f12b357c6bfd2e78195cdbd69add3b04c58402e5
Everlyne Mosomi <evemosomi@gmail.com>
2023-01-04 22:53:25 -0800

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * factorial-if n<0 returns error if n>o factorial of n
+ * @n:number
+ * Return:factorial of a number
+ */
+int factorial(int n)
+{
+	int fact = n;
+
+	if (n < 0)
+		return (-1);
+	else if (n >= 0 && n <= 1)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
