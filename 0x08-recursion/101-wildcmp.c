923a3d1934512a0b92c96dcb12218f44afcef31a
Everlyne Mosomi <evemosomi@gmail.com>
2023-01-05 18:07:51 -0800

    101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,13 +7,13 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0')
-	{
-		if (*s2 != '\0' && *s2 == '*')
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
 		{
-			return (wildcmp(s1, s2 + 1));
+		return (0);
 		}
-		return (*s2 == '\0');
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (0);
 	}
 	if (*s2 == '*')
 	{
