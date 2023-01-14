ead8f3fb3f6dd24a8abdd88df44425494e895c05
Everlyne Mosomi <evemosomi@gmail.com>
2023-01-14 09:05:24 -0800

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints
+ * @argc:counts
+ * @argv:array
+ * Return:0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
