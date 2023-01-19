4beddf4924a77450b748ec74add9ecb6368bd05c
Everlyne Mosomi <evemosomi@gmail.com>
2023-01-19 15:19:37 -0800

    3 in 1

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,25 @@
+#include "3-calc_h"
+/**
+ * get_op_func-fn pointer
+ * @s:the op given by the user
+ * Return:pointer
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (ops[i].op)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include "3-calc.h"
+/**
+ * op_add-sum
+ * @a:1st no
+ * @b:2nd no
+ * Return:result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub-diff
+ * @a:1st no
+ * @b:2n no
+ * Return:diff
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul-product
+ * @a:1st
+ * @b:2nd
+ * Return:product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div-division
+ * @a:1st
+ * @b:2nd
+ * Return:division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod-remainder
+ * @a:1st
+ * @b:2nd
+ * Return:remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
+
+
+
